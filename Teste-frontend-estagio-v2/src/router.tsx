2495c918c58efe86c2cd8027cef7b44460510889
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-06-23 17:33:56 -0300

    feat: implement routes

--- Teste-frontend-estagio-v2/src/router.tsx
+++ Teste-frontend-estagio-v2/src/router.tsx
@@ -1,7 +1,16 @@
 import { useRoutes } from "react-router-dom"
+import MapView from "./pages/MapView/mapView"
+import StatusHistory from "./pages/StatusHistory/StatusHistory"
 
 export const Router = () => {
     return useRoutes([
-        
+        {
+            path: '/',
+            element: <MapView />
+        },
+        {
+            path: '/states:equipamentId',
+            element: <StatusHistory />
+        }
     ])
 }
