9ba087619a690458e88c152ccfe08be812b3370d
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-06-23 17:34:35 -0300

    feat: import routes from Router

--- Teste-frontend-estagio-v2/src/App.tsx
+++ Teste-frontend-estagio-v2/src/App.tsx
@@ -1,19 +1,11 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { Router } from "./router";
 
 function App() {
   return (
     <BrowserRouter>
-      <MapContainer center={[-19.264235, -46.092436]} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[-19.264235, -46.092436]}>
-          <Popup>Posição</Popup>
-        </Marker>
-      </MapContainer>
+      <Router />
     </BrowserRouter>
   );
 }
